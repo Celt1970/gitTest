7b671a48a1861b9e424d6a2c054b534c21c25f96
Celt1970 <borisov-juriy1992@rambler.ru>
2018-06-08 19:40:08 +0300

    added new class AbstractClass

---
+++ GitTest/AbstractClass.swift
@@ -0,0 +1,17 @@
+//
+//  AbstractClass.swift
+//  GitTest
+//
+//  Created by Yuriy borisov on 08.06.2018.
+//  Copyright © 2018 Yuriy borisov. All rights reserved.
+//
+
+import Foundation
+
+class AbstractClass {
+    var absField = "field"
+    
+    func absMethod() {
+        print(absField)
+    }
+}
