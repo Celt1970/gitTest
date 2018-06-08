4f8334d37652a85301a2b39fc9502418a198ad37
Celt1970 <borisov-juriy1992@rambler.ru>
2018-06-08 16:21:26 +0300

    Hellow world added to ViewController

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
-        
+        print("Hello world!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
